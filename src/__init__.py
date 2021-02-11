60151c2570e2dea84a6d00c0239d8cb47598aee3
CamClrt <camille.clarret@gmail.com>
2021-02-11 20:07:18 +0100

    make it better

--- src/__init__.py
+++ src/__init__.py
@@ -1,28 +1,20 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
 from src.models.kanye_rest_api import KanyeRestApi
 from src.models.random_cat_api import RandomCatApi
 
 
-def create_app(test_config=None):
+# Init dotenv
+load_dotenv()
+
+
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY="dev")
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    app.config.from_mapping(SECRET_KEY=os.environ.get("SECRET_KEY") or "dev")
 
     # a simple index page
     @app.route("/")
